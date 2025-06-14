1976ae45304a2f6d72583dd9ca505961fa6a1d22
pedrohenriquemop <madeirapedrohenrique1112@gmail.com>
2025-06-13 22:13:35 -0300

    part1 almost working

--- chat.py
+++ chat.py
@@ -5,6 +5,7 @@ from enum import Enum
 from typing import List, Literal, Tuple
 import threading
 import time
+import traceback
 
 PORT = 51511
 HASH_LENGTH = 16
@@ -140,7 +141,7 @@ class GeneralResponse:
 
 
 class PeerList(GeneralResponse):
-    def __init__(self, known_peers_amount: int, known_peers: List[str]):
+    def __init__(self, known_peers_amount: int, known_peers: List[Tuple[str, int]]):
         super().__init__(MessageCode.PEER_RESPONSE)
 
         if known_peers_amount != len(known_peers):
@@ -152,28 +153,44 @@ class PeerList(GeneralResponse):
         self.known_peers = known_peers
 
     def pack(self):
-        return struct.pack("!BI", self.code.value, self.known_peers_amount) + b"".join(
-            struct.pack("!I", peer) for peer in self.known_peers
+        packed_code = struct.pack("!B", self.code.value)
+        packed_amount = struct.pack("!I", self.known_peers_amount)
+        packed_peers_data = b"".join(
+            socket.inet_aton(peer_ip) for peer_ip, _ in self.known_peers
         )
+
+        return packed_code + packed_amount + packed_peers_data
 
     @classmethod
     def unpack(cls, data: bytes):
         if len(data) < 5:
             raise ValueError("Invalid data size for PeerList unpacking")
 
-        _, known_peers_amount = struct.unpack("!BI", data[:5])
-        if len(data) != 5 + known_peers_amount * 4:
-            raise ValueError("Data size does not match known peers amount")
+        code_value, known_peers_amount = struct.unpack("!BI", data[:5])
 
-        known_peers = []
+        if code_value != MessageCode.PEER_RESPONSE.value:
+            raise ValueError(
+                f"Message code mismatch for PeerList. Expected {MessageCode.PEER_RESPONSE.value}, got {code_value}."
+            )
+
+        expected_total_size = 5 + known_peers_amount * 4
+        if len(data) != expected_total_size:
+            raise ValueError(
+                f"Data size ({len(data)}) does not match expected size ({expected_total_size}) based on known peers amount ({known_peers_amount})."
+            )
+
+        unpacked_peers_list: List[str] = []
 
         for i in range(known_peers_amount):
             start = 5 + i * 4
             end = start + 4
-            peer = struct.unpack("!I", data[start:end])[0]
-            known_peers.append(peer)
 
-        return cls(known_peers_amount, known_peers)
+            ip_bytes = data[start:end]
+
+            peer_ip_str = socket.inet_ntoa(ip_bytes)
+            unpacked_peers_list.append((peer_ip_str, PORT))
+
+        return cls(known_peers_amount, unpacked_peers_list)
 
 
 class ArchiveResponse(GeneralResponse):
@@ -220,7 +237,7 @@ class P2PChatEngine:
         self.lock = threading.Lock()
         self.server_thread = None
         self.sender_thread = None
-        self.active_connections: dict[str, Tuple[str, int]] = {}
+        self.active_connections: dict[Tuple[str, int], socket.socket] = {}
 
     def start(self):
         print(f"Starting P2P Chat Engine on {self.ip}:{self.port}")
@@ -270,9 +287,8 @@ class P2PChatEngine:
             while True:
                 header_byte = conn_sock.recv(1)
 
-                print(f"Received byte: {header_byte[0]}")
                 message_type = Utils.get_message_type_from_code(header_byte[0])
-                print(f"Message type: {message_type}")
+                print(f"> Received message type: {message_type}")
 
                 if message_type == MessageCode.PEER_REQUEST.name:
                     peer_response = PeerList(
@@ -280,10 +296,31 @@ class P2PChatEngine:
                         known_peers=list(self.active_connections.keys()),
                     )
                     conn_sock.sendall(peer_response.pack())
+
+                if message_type == MessageCode.PEER_RESPONSE.name:
+                    length = conn_sock.recv(4)
+                    if len(length) < 4:
+                        print("> Received incomplete length for PeerList")
+                        continue
+                    known_peers_amount = struct.unpack("!I", length)[0]
+                    peers_data = conn_sock.recv(known_peers_amount * 4)
+                    if len(peers_data) < known_peers_amount * 4:
+                        print("> Received incomplete peer data")
+                        continue
+                    peer_list_response = PeerList.unpack(
+                        header_byte + length + peers_data
+                    )
+                    print("> Received PeerList:", peer_list_response.known_peers)
+
+                    for peer_ip, _ in peer_list_response.known_peers:
+                        if self.ip != peer_ip and peer_ip not in [
+                            key[0] for key in self.active_connections.keys()
+                        ]:
+                            self.connect_to_peer(peer_ip)
+
         except Exception as e:
-            print(
-                f"Error in listener for {addr}: {e}"
-            )
+            print(f"Error in listener for {addr}: {e}")
+            traceback.print_stack()
         finally:
             conn_sock.close()
             if addr in self.active_connections:
@@ -313,7 +350,7 @@ class P2PChatEngine:
             raise Exception("Cannot connect to self")
 
         try:
-            print(f"Connecting to peer: {peer_ip}:{PORT}")
+            print(f"! Connecting to peer: {peer_ip}:{PORT}")
             peer_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             peer_sock.connect((peer_ip, PORT))
             print(f"Connected to {peer_ip}:{PORT}")
